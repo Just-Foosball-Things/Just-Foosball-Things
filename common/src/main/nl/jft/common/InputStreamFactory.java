1f953702a18d5da72dde564db52825627e745643
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-01-20 20:42:38 +0100

    Fix javadoc

--- common/src/main/nl/jft/common/InputStreamFactory.java
+++ common/src/main/nl/jft/common/InputStreamFactory.java
@@ -8,5 +8,12 @@ import java.io.InputStream;
  * @author Oscar de Leeuw
  */
 public interface InputStreamFactory {
+    /**
+     * Creates a new {@code InputStream}.
+     *
+     * @param file The {@code File} on which an {@code InputStream} should be created.
+     * @return An {@code InputStream}.
+     * @throws IOException When an exception occurs during stream creation.
+     */
     InputStream createStream(File file) throws IOException;
 }
