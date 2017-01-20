e9ce2bcbb996cb14efd4ff654de772f5092d0049
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 21:29:58 +0100

    Add javadoc + unit tests for ParticipantType.

--- logic/src/main/nl/jft/logic/participant/ParticipantType.java
+++ logic/src/main/nl/jft/logic/participant/ParticipantType.java
@@ -1,11 +1,22 @@
 package nl.jft.logic.participant;
 
 /**
+ * A {@code ParticipantType} defines what kind of type a {@link Participant} is.
+ *
  * @author Lesley
  */
 public enum ParticipantType {
 
+    /**
+     * Indicates that a {@link Participant} is playing on his/her own. The implementation is, usually,
+     * {@link nl.jft.logic.participant.impl.User}.
+     */
     SOLO,
+
+    /**
+     * Indicates that a {@link Participant} is playing with other {@code Participants}. The implementation is, usually,
+     * {@link nl.jft.logic.participant.impl.Team}.
+     */
     TEAM
 
 }
