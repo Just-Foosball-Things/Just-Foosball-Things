bafa4e2d89ab60c46d52cde9b5a089ede82725cc
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-01-20 14:38:43 +0100

    Created a base for the Participant interface.

--- logic/src/main/nl/jft/logic/participant/Participant.java
+++ logic/src/main/nl/jft/logic/participant/Participant.java
@@ -1,8 +1,17 @@
 package nl.jft.logic.participant;
 
 /**
+ * The {@code Participant} interface defines a participant that can partake in {@link nl.jft.logic.match.Match Matches} and {@link nl.jft.logic.tournament.Tournament Tournamements}.
+ * A {@code Participant} is stored in the database and thus should be //TODO add identifiable link when implemented.
+ *
  * @author Lesley
+ * @author Oscar de Leeuw
  */
 public interface Participant {
-
+    /**
+     * Gets the name of the {@code Participant}.
+     *
+     * @return A {@code String} that represents the name of the {@code Participant}.
+     */
+    String getName();
 }
