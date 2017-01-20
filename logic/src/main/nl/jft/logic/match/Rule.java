09daadc796d1c8e90e8afac1356ee812b42d5a95
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 21:43:48 +0100

    Add unit tests + implementation for Rule.

--- logic/src/main/nl/jft/logic/match/Rule.java
+++ logic/src/main/nl/jft/logic/match/Rule.java
@@ -1,8 +1,27 @@
 package nl.jft.logic.match;
 
+import nl.jft.common.Identifiable;
+import nl.jft.common.util.Arguments;
+
 /**
  * @author Lesley
  */
-public class Rule {
+public class Rule implements Identifiable {
 
+    private final int id;
+    private final String description;
+
+    public Rule(int id, String description) {
+        this.id = id;
+        this.description = Arguments.requireNotEmpty(description);
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
