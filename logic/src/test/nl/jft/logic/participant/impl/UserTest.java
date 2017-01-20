980348855f0940642f50b92a36a664c4e6196a16
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 15:54:16 +0100

    Added beginning tests for Goal and added an implementation of Goal.

--- logic/src/test/nl/jft/logic/participant/impl/UserTest.java
+++ logic/src/test/nl/jft/logic/participant/impl/UserTest.java
@@ -3,7 +3,7 @@ package nl.jft.logic.participant.impl;
 import nl.jft.logic.util.LogicTestUtil;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * @author Lesley
@@ -18,6 +18,39 @@ public class UserTest {
     @Test(expected = IllegalArgumentException.class)
     public void construct_emptyUsername_throwsException() {
         User user = new User("");
+    }
+
+    @Test
+    public void equals_sameObjects_returnsTrue() {
+        User user1 = LogicTestUtil.makeDefaultUser();
+        User user2 = LogicTestUtil.makeDefaultUser();
+
+        assertTrue(user1.equals(user2));
+    }
+
+    @Test
+    public void equals_otherObject_returnsFalse() {
+        User user1 = LogicTestUtil.makeDefaultUser();
+        String user2 = "user2";
+
+        assertFalse(user1.equals(user2));
+    }
+
+    @Test
+    public void equals_nullObject_returnsFalse() {
+        User user1 = LogicTestUtil.makeDefaultUser();
+        User user2 = null;
+
+        assertFalse(user1.equals(user2));
+    }
+
+    @Test
+    public void hashCode_whenCalled_returnsHashCode() {
+        User user1 = LogicTestUtil.makeDefaultUser();
+        User user2 = LogicTestUtil.makeDefaultUser();
+
+        boolean result = user1.hashCode() == user2.hashCode();
+        assertTrue(result);
     }
 
     @Test
