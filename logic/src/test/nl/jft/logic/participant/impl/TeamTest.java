a90bf754f8b350efb73f669be03d9bb2669da48e
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-01-20 15:28:47 +0100

    Created a base of the Team class.

--- logic/src/test/nl/jft/logic/participant/impl/TeamTest.java
+++ logic/src/test/nl/jft/logic/participant/impl/TeamTest.java
@@ -1,8 +1,53 @@
 package nl.jft.logic.participant.impl;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
  * @author Lesley
+ * @author Oscar de Leeuw
  */
 public class TeamTest {
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
 
+    @Test
+    public void construct_nullTeamname_throwsException() throws Exception {
+        expectedException.expect(NullPointerException.class);
+
+        Team team = new Team(-1, null);
+    }
+
+    @Test
+    public void construct_emptyTeamname_throwsException() throws Exception {
+        expectedException.expect(IllegalArgumentException.class);
+
+        Team team = new Team(-1, "");
+    }
+
+    @Test
+    public void getName_withDefaultName_returnsName() throws Exception {
+        Team team = makeTeam(); //default name is "team".
+
+        String expected = "team";
+        String actual = team.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getId_withDefaultId_returnsId() throws Exception {
+        Team team = makeTeam(); //default id is 1.
+
+        int expected = 1;
+        int actual = team.getId();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    private Team makeTeam() {
+        return new Team(1, "team");
+    }
 }
