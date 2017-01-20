6faf87d601f1c25e5e2bb4965695c88ac1c82fe7
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 21:46:48 +0100

    Add javadoc to Rule class.

--- logic/src/main/nl/jft/logic/match/Rule.java
+++ logic/src/main/nl/jft/logic/match/Rule.java
@@ -4,6 +4,8 @@ import nl.jft.common.Identifiable;
 import nl.jft.common.util.Arguments;
 
 /**
+ * A {@code Rule} defines what should and should not be allowed during a {@link Match}.
+ *
  * @author Lesley
  */
 public class Rule implements Identifiable {
@@ -11,6 +13,13 @@ public class Rule implements Identifiable {
     private final int id;
     private final String description;
 
+    /**
+     * Initiates a new {@code Rule} using the given {@code id} and {@code description}.
+     *
+     * @param id          The {@code id} of this {@code Rule}, used by external layers.
+     * @param description The {@code description} of this {@code Rule}, this contains the actual definition
+     *                    of this {@code Rule}, should not be {@code null} or empty.
+     */
     public Rule(int id, String description) {
         this.id = id;
         this.description = Arguments.requireNotEmpty(description);
@@ -21,7 +30,13 @@ public class Rule implements Identifiable {
         return id;
     }
 
+    /**
+     * Gets the {@code description} of this {@code Rule}.
+     *
+     * @return The {@code description} of this {@code Rule}.
+     */
     public String getDescription() {
         return description;
     }
+
 }
