d60e6e92f5e30880e1266e54e4c71229fe0d3e10
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 15:06:06 +0100

    Add interface definition of an Identifiable.

---
+++ common/src/main/nl/jft/common/Identifiable.java
@@ -0,0 +1,17 @@
+package nl.jft.common;
+
+/**
+ * Represents an {@link Object} which can be identified by an id (see {@code getId()}).
+ *
+ * @author Lesley Vente
+ */
+public interface Identifiable {
+
+    /**
+     * Gets the id of this {@code Identifiable}.
+     *
+     * @return The id of this {@code Identifiable}.
+     */
+    int getId();
+
+}
