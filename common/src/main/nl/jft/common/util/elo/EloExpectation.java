0e3195b84df385b088972ed4bb974f6cf3b53448
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 20:51:34 +0100

    Global things

---
+++ common/src/main/nl/jft/common/util/elo/EloExpectation.java
@@ -0,0 +1,19 @@
+package nl.jft.common.util.elo;
+
+/**
+ * @author Lesley
+ */
+public class EloExpectation {
+
+    private double elo1;
+    private double elo2;
+
+    private int goals1;
+    private int goals2;
+
+    private int maxGoals;
+
+    private double delta1;
+    private double delta2;
+
+}
---
+++ common/src/main/nl/jft/common/util/elo/EloExpectationLoader.java
@@ -0,0 +1,12 @@
+package nl.jft.common.util.elo;
+
+import java.util.List;
+
+/**
+ * @author Lesley
+ */
+public interface EloExpectationLoader {
+
+    List<EloExpectation> load();
+
+}
