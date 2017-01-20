3d3c3a7176e4a29f77cd92a766e93d3be329d163
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 15:41:58 +0100

    Added beginning tests for Goal and added an implementation of Goal.

--- logic/src/main/nl/jft/logic/match/Goal.java
+++ logic/src/main/nl/jft/logic/match/Goal.java
@@ -1,8 +1,43 @@
 package nl.jft.logic.match;
 
+import nl.jft.common.Identifiable;
+import nl.jft.logic.participant.Participant;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 /**
+ * A {@code Goal} represents a {@code Goal} made by a {@link nl.jft.logic.participant.Participant} in a {@link Match}.
+ * It contains metadata about the {@code Goal} such as who scored it, and when it was scored.
+ *
  * @author Lesley
  */
-public class Goal {
+public class Goal implements Identifiable {
+
+    private final int id;
+    private final Participant participant;
+    private final LocalDateTime time;
+
+    /**
+     * Initializes a new Goal using the given id, participant and time.
+     *
+     * @param id          The {@code id} (dependant on an external system such as a database) of this {@code Goal}.
+     * @param participant The {@code participant} who scored this {@code Goal}. Can be {@code null} to indicate the scorer is unknown.
+     * @param time        The {@code time} at which {@code Goal} was scored, should not be null.
+     */
+    public Goal(int id, Participant participant, LocalDateTime time) {
+        this.id = id;
+        this.participant = participant;
+        this.time = Objects.requireNonNull(time);
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public Participant getParticipant() {
+        return participant;
+    }
 
 }
--- logic/src/test/nl/jft/logic/match/GoalTest.java
+++ logic/src/test/nl/jft/logic/match/GoalTest.java
@@ -1,8 +1,76 @@
 package nl.jft.logic.match;
 
+import nl.jft.logic.participant.Participant;
+import nl.jft.logic.participant.impl.User;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Lesley
  */
 public class GoalTest {
+
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void construct_nullTime_throwsException() {
+        expectedException.expect(NullPointerException.class);
+
+        Participant participant = makeUser();
+        Goal goal = new Goal(0, participant, null);
+    }
+
+    @Test
+    public void getId_whenCalled_returnsId() {
+        Goal goal = makeGoalWithUser(); // Default id = 0.
+
+        int expected = 0;
+        int actual = goal.getId();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getParticipant_nullParticipant_returnsNull() {
+        Goal goal = makeGoalNoParticipant();
+
+        Participant expected = null;
+        Participant actual = goal.getParticipant();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getParticipant_userParticipant_returnsUser() {
+        Goal goal = makeGoalWithUser();
+
+        Participant expected = new User("username");
+        Participant actual = goal.getParticipant();
+
+        assertEquals(expected, actual);
+    }
+
+    private Goal makeGoalNoParticipant() {
+        return new Goal(0, null, makeLocalDateTime());
+    }
+
+    private Goal makeGoalWithUser() {
+        return new Goal(0, makeUser(), makeLocalDateTime());
+    }
+
+    private Participant makeUser() {
+        return new User("username");
+    }
+
+    private LocalDateTime makeLocalDateTime() {
+        return LocalDateTime.of(2017, Month.JANUARY, 1, 12, 1, 1);
+    }
 
 }
