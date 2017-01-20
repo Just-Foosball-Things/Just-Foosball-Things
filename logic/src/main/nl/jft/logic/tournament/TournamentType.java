7237645c5dea141026d1b6ca8569c716ffcd9673
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 21:32:42 +0100

    Add javadoc + unit tests for TournamentType.

--- logic/src/main/nl/jft/logic/tournament/TournamentType.java
+++ logic/src/main/nl/jft/logic/tournament/TournamentType.java
@@ -1,10 +1,17 @@
 package nl.jft.logic.tournament;
 
 /**
+ * A {@code TournamentType} defines how {@link nl.jft.logic.match.Match matches} should be schedules
+ * in a {@link Tournament}.
+ *
  * @author Lesley
  */
 public enum TournamentType {
 
+    /**
+     * {@code KNOCKOUT} indicates that {@link nl.jft.logic.participant.Participant participants} should be kicked out
+     * of a {@link Tournament} after they lose a {@link nl.jft.logic.match.Match}.
+     */
     KNOCKOUT
 
 }
