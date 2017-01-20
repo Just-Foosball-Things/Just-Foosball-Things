cb9a9ec1811ec023b39e87539cae85f423251303
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 16:04:25 +0100

    Add factory methods to TestUtil and fix broken tests in GoalTest.

--- logic/src/test/nl/jft/logic/match/GoalTest.java
+++ logic/src/test/nl/jft/logic/match/GoalTest.java
@@ -1,13 +1,10 @@
 package nl.jft.logic.match;
 
 import nl.jft.logic.participant.Participant;
-import nl.jft.logic.participant.impl.User;
+import nl.jft.logic.util.LogicTestUtil;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
-import java.time.LocalDateTime;
-import java.time.Month;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,15 +20,15 @@ public class GoalTest {
     public void construct_nullTime_throwsException() {
         expectedException.expect(NullPointerException.class);
 
-        Participant participant = makeUser();
+        Participant participant = LogicTestUtil.makeDefaultUser();
         Goal goal = new Goal(0, participant, null);
     }
 
     @Test
     public void getId_whenCalled_returnsId() {
-        Goal goal = makeGoalWithUser(); // Default id = 0.
+        Goal goal = LogicTestUtil.makeGoalWithUser(); // Default id = 0.
 
-        int expected = 0;
+        int expected = -1;
         int actual = goal.getId();
 
         assertEquals(expected, actual);
@@ -39,7 +36,7 @@ public class GoalTest {
 
     @Test
     public void getParticipant_nullParticipant_returnsNull() {
-        Goal goal = makeGoalNoParticipant();
+        Goal goal = LogicTestUtil.makeGoalNoParticipant();
 
         Participant expected = null;
         Participant actual = goal.getParticipant();
@@ -49,28 +46,12 @@ public class GoalTest {
 
     @Test
     public void getParticipant_userParticipant_returnsUser() {
-        Goal goal = makeGoalWithUser();
+        Goal goal = LogicTestUtil.makeGoalWithUser();
 
-        Participant expected = new User("username");
+        Participant expected = LogicTestUtil.makeDefaultUser();
         Participant actual = goal.getParticipant();
 
         assertEquals(expected, actual);
-    }
-
-    private Goal makeGoalNoParticipant() {
-        return new Goal(0, null, makeLocalDateTime());
-    }
-
-    private Goal makeGoalWithUser() {
-        return new Goal(0, makeUser(), makeLocalDateTime());
-    }
-
-    private Participant makeUser() {
-        return new User("username");
-    }
-
-    private LocalDateTime makeLocalDateTime() {
-        return LocalDateTime.of(2017, Month.JANUARY, 1, 12, 1, 1);
     }
 
 }
