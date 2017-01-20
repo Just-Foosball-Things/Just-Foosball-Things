859b9ff7c4dd860aba62be6a3784ba9b26b47e5b
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-01-20 21:25:35 +0100

    Add javadoc + unit tests to MatchType.

--- logic/src/main/nl/jft/logic/match/MatchType.java
+++ logic/src/main/nl/jft/logic/match/MatchType.java
@@ -1,11 +1,22 @@
 package nl.jft.logic.match;
 
 /**
+ * A {@code MatchType} defines how a {@link Match} should be treated.
+ *
  * @author Lesley
  */
 public enum MatchType {
 
+    /**
+     * A {@code RATED} {@code MatchType} indicates that a {@link nl.jft.logic.participant.impl.User}
+     * should change its {@link nl.jft.logic.participant.Elo} after finishing a {@link Match}.
+     */
     RATED,
+
+    /**
+     * A {@code FRIENDLY} {@code MatchType} indicates that a {@link nl.jft.logic.participant.impl.User}
+     * should not change its {@link nl.jft.logic.participant.Elo} after finishing a {@link Match}.
+     */
     FRIENDLY
 
 }
