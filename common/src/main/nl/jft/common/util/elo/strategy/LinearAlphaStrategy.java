99f4b6528ee6d4c0cb7d43ca822e672bd747b687
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-01-24 11:51:14 +0100

    Added LinearAlphaStrategy documentation and tests.

--- common/src/main/nl/jft/common/util/elo/strategy/LinearAlphaStrategy.java
+++ common/src/main/nl/jft/common/util/elo/strategy/LinearAlphaStrategy.java
@@ -3,6 +3,16 @@ package nl.jft.common.util.elo.strategy;
 import nl.jft.common.util.elo.EloCalculationStrategy;
 
 /**
+ * The {@code LinearAlphaStrategy} is an implementation of {@code EloCalculationStrategy}.
+ * It approaches the evaluation of a foosball game linearly.
+ *
+ * The constant factors used are:
+ * <li>
+ *     <ul>{@link #K_FACTOR}</ul>
+ *     <ul>{@link #RATING_FACTOR}</ul>
+ *     <ul>{@link #INITIAL_SCORE}</ul>
+ * </li>
+ *
  * @author Oscar de Leeuw
  */
 public class LinearAlphaStrategy implements EloCalculationStrategy {
@@ -21,12 +31,27 @@ public class LinearAlphaStrategy implements EloCalculationStrategy {
         return currentRating + K_FACTOR * (actualScore - expectedScore);
     }
 
-    private double getExpectedScore(double currentRating, double opponentRating) {
+    /**
+     * Gets the expected score for two given ratings.
+     *
+     * @param currentRating  The current rating of the player for which to calculate the expected score.
+     * @param opponentRating The current rating of the opposing player.
+     * @return The expected outcome of the game.
+     */
+    public double getExpectedScore(double currentRating, double opponentRating) {
         return 1 / (1 + Math.pow(10, (opponentRating - currentRating) / RATING_FACTOR));
     }
 
-    private double getActualScore(int goalsScored, int goalsConceded, int maxGoals) {
-        return INITIAL_SCORE + (goalsScored - goalsConceded) / (2 * maxGoals);
+    /**
+     * Gets the actual score from a game.
+     *
+     * @param goalsScored   The amount of goals scored.
+     * @param goalsConceded The amount of goals conceded.
+     * @param maxGoals      The maximum amount of goals for a single game.
+     * @return The actual score of the the game.
+     */
+    public double getActualScore(int goalsScored, int goalsConceded, int maxGoals) {
+        return INITIAL_SCORE + (((double) goalsScored - (double) goalsConceded) / (2d * (double) maxGoals));
     }
 
 }
