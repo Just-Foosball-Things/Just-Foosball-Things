acf816f64ded49e61c57eb85d10718203bf9c789
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-01-26 15:10:46 +0100

    Added steps 4 and 5 of Glickman to GlickoCalculator.

--- common/src/main/nl/jft/common/glicko/StandardGlickoCalculator.java
+++ common/src/main/nl/jft/common/glicko/StandardGlickoCalculator.java
@@ -68,7 +68,7 @@ public class StandardGlickoCalculator implements GlickoCalculator {
     }
 
     /**
-     * Evaluates the v function as described in step 3 of Glickman's paper: <br>
+     * Calculates the value of v as described in step 3 of Glickman's paper: <br>
      * {@code v = 1d / (g(phiJ)^2 * E(mu,muJ,phiJ) * (1 - E(mu,muj,phiJ)))} <br>
      * This implementation does not support evaluating multiple results at the same time.
      * Within the JFT-application every result will immediately be processed
@@ -86,13 +86,113 @@ public class StandardGlickoCalculator implements GlickoCalculator {
         return 1d / (Math.pow(gPhiJ, 2d) * e * (1d - e));
     }
 
-
+    /**
+     * Calculates the value of {@code delta} as described in step 4 of Glickman's paper: <br>
+     * {@code delta = v * g(phiJ) * (sJ - e(mu,muJ,phiJ))} <br>
+     * This implementation does not support evaluating multiple results simultaneously.
+     *
+     * @param rating            The rating of the player.
+     * @param opponentRating    The rating of the opposing player.
+     * @param opponentDeviation The deviation of the opposing player.
+     * @param actualScore       The actual score of the game.
+     * @return a {@code double} that represents {@code delta}.
+     */
     public double delta(double rating, double opponentRating, double opponentDeviation, double actualScore) {
         double v = v(rating, opponentRating, opponentDeviation);
         double gPhiJ = g(opponentDeviation);
         double e = e(rating, opponentRating, opponentDeviation);
 
         return v * (gPhiJ * (actualScore - e));
+    }
+
+    /**
+     * Calculates the value of {@code a} as described in step 5.1 of Glickman's paper: <br>
+     * {@code a = ln(sigma^2)} where sigma is the volatility of the player
+     *
+     * @param volatility The volatility of the player.
+     * @return a {@code double} that represents {@code a}.
+     */
+    public double a(double volatility) {
+        return Math.log(Math.pow(volatility, 2));
+    }
+
+    /**
+     * Evaluates the function {@code f} as described in step 5.1 of Glickman's paper: <br>
+     * {@code f(x) = ((e^x * (delta^2 - phi^2 - v - e^x)) / (2*(phi^2+v+e^x)^2)) - ((x-a)/tau^2)} <br>
+     * Function is known to be prone to rounding errors as evaluating the function through Google,
+     * or Wolfram Alpha, yields slightly different results.
+     *
+     * @param x     The parameter for which to evaluate the function {@code f}.
+     * @param delta The value of {@link #delta(double, double, double, double)}.
+     * @param phi   The value of {@link #phi(double)}.
+     * @param v     The value of {@link #v(double, double, double)}.
+     * @param a     The value of {@link #a(double)}.
+     * @param tau   The value of {@code tau}.
+     * @return a {@code double} that represents {@code f(x)}.
+     */
+    public double f(double x, double delta, double phi, double v, double a, double tau) {
+        double numerator = Math.exp(x) * (Math.pow(delta, 2d) - Math.pow(phi, 2d) - v - Math.exp(x));
+        double denominator = 2d * Math.pow((Math.pow(phi, 2d) + v + Math.exp(x)), 2d);
+
+        double firstTerm = numerator / denominator;
+        double secondTerm = (x - a) / Math.pow(tau, 2d);
+
+        return firstTerm - secondTerm;
+    }
+
+    /**
+     * Calculates the new volatility as described in step 5.2-5.5 in Glickman's paper.
+     * This method iterates through the iterative algorithm described in step 5.
+     *
+     * @param delta       The value of {@link #delta(double, double, double, double)}.
+     * @param phi         The value of {@link #phi(double)}.
+     * @param v           The value of {@link #v(double, double, double)}.
+     * @param a           The value of {@link #a(double)}.
+     * @param tau         The value of {@code tau}.
+     * @param convergence the value to which the iterative algorithm should converge.
+     * @return a {@code double} that represents the new volatility of a player.
+     */
+    public double newSigma(double delta, double phi, double v, double a, double tau, double convergence) {
+        double bigA = a;
+        double bigB;
+
+        //step 5.2
+
+        if (Math.pow(delta, 2) > Math.pow(phi, 2) + v) {
+            bigB = Math.log(Math.pow(delta, 2) + Math.pow(phi, 2) + v);
+        } else {
+            int k = 1;
+            while (f((a - k * tau), delta, phi, v, a, tau) < 0) {
+                k++;
+            }
+            bigB = a - k * tau;
+        }
+
+        //step 5.3
+
+        double fOfA = f(bigA, delta, phi, v, a, tau);
+        double fOfB = f(bigB, delta, phi, v, a, tau);
+
+        //step 5.4
+
+        while (Math.abs(bigA - bigB) > convergence) {
+            double bigC = bigA + (bigA - bigB) * fOfA / (fOfB - fOfA);
+            double fOfC = f(bigC, delta, phi, v, a, tau);
+
+            if (fOfC * fOfB < 0) {
+                bigA = bigB;
+                fOfA = fOfB;
+            } else {
+                fOfA = fOfA / 2d;
+            }
+
+            bigB = bigC;
+            fOfB = fOfC;
+        }
+
+        //step 5.5
+
+        return Math.exp(bigA/2);
     }
 
     @Override
--- common/src/test/nl/jft/common/glicko/StandardGlickoCalculatorTest.java
+++ common/src/test/nl/jft/common/glicko/StandardGlickoCalculatorTest.java
@@ -118,6 +118,14 @@ public class StandardGlickoCalculatorTest {
     }
 
     @Test
+    public void phi_with200Argument_returnsDouble() throws Exception {
+        double expected = 1.1513;
+        double actual = calculator.phi(200);
+
+        assertEquals(expected, actual, 0.0001d);
+    }
+
+    @Test
     public void g_with500Argument_returnsDouble() throws Exception {
         double expected = 0.53314574144;
         double actual = calculator.g(500);
@@ -245,4 +253,146 @@ public class StandardGlickoCalculatorTest {
 
         assertEquals(expected, actual, 0.0000001d);
     }
+
+    @Test
+    public void delta_with1500Rating2000Rating350Deviation_returnsDouble() throws Exception {
+        double expected = 11.747957867219615;
+        double actual = calculator.delta(1500, 2000, 350, 1);
+
+        //v = 20.1181494346357595
+        //e = 0.1272232377
+        //g = 0.6690694125804828
+
+        assertEquals(expected, actual, 0.0000001d);
+    }
+
+    @Test
+    public void delta_with1000Rating3000Rating350Deviation_returnsDouble() throws Exception {
+        double expected = 1655.18165406207357949;
+        double actual = calculator.delta(1000, 3000, 350, 0.5);
+
+        //v = 4952.18257166890839;
+        //e = 0.00045129138
+        //g = 0.6690694125804828
+
+        assertEquals(expected, actual, 0.0001d);
+    }
+
+    @Test
+    public void delta_with1500Rating1500Rating200Deviation_returnsDouble() throws Exception {
+        double expected = -2.3688781399635;
+        double actual = calculator.delta(1500, 1500, 200, 0);
+
+        //v = 5.61158364199695
+        //e = 0.5
+        //g = 0.8442815045061
+
+        assertEquals(expected, actual, 0.0000001d);
+    }
+
+    @Test
+    public void a_with006Volatility_returnsDouble() throws Exception {
+        double expected = -10.231991619;
+        double actual = calculator.a(0.006);
+
+        assertEquals(expected, actual, 0.0000001d);
+    }
+
+    @Test
+    public void a_with007Volatility_returnsDouble() throws Exception {
+        double expected = -9.92369025985364;
+        double actual = calculator.a(0.007);
+
+        assertEquals(expected, actual, 0.0001d);
+    }
+
+    @Test
+    public void a_with005Volatility_returnsDouble() throws Exception {
+        double expected = -10.59663473309607;
+        double actual = calculator.a(0.005);
+
+        assertEquals(expected, actual, 0.0000001d);
+    }
+
+    @Test
+    public void f_testOne_returnsDouble() throws Exception {
+        double expected = -19.7201890814611;
+        double actual = calculator.f(1, 11.747957867219615, 1.43911562315, 20.1181494346357595, -10.231991619, 0.75);
+
+        //delta = 11.747957867219615
+        //v = 20.1181494346357595
+        //phi = 1.43911562315
+        //a = -10.231991619
+        //tau = 0.75
+
+        assertEquals(expected, actual, 0.0000001d);
+    }
+
+    @Test
+    public void f_testTwo_returnsDouble() throws Exception {
+        double expected = -19.26875089204392;
+        double actual = calculator.f(1, 1655.18165406207357949, 2.01476187242, 4952.18257166890839, -9.92369025985364, 0.75);
+
+        //delta = 1655.18165406207357949
+        //v = 4952.18257166890839
+        //phi = 2.01476187242
+        //a = -9.92369025985364
+        //tau = 0.75
+
+        assertEquals(expected, actual, 0.0001d);
+    }
+
+    @Test
+    public void f_testThree_returnsDouble() throws Exception {
+        double expected = -19.0668560505;
+        double actual = calculator.f(0.1, -2.368878, 2.87823, 5.6115836, -10.5966347, 0.75);
+
+        //delta = -2.3688781399635
+        //v = 5.61158364199695
+        //phi = 2.87823124631
+        //a = -10.59663473309607
+        //tau = 0.75
+
+        assertEquals(expected, actual, 0.1d);
+    }
+
+    @Test
+    public void newSigma_testOne_returnsDouble() throws Exception {
+        double expected = 0.06;
+        double actual = calculator.newSigma(-2.3688781399635, 2.87823124631, 5.61158364199695, -5.62682, 0.75, 0.000001);
+
+        //delta = -2.3688781399635
+        //v = 5.61158364199695
+        //phi = 2.87823124631
+        //a = -10.231991619
+        //tau = 0.75
+        //convergence = 0.000001
+
+        assertEquals(expected, actual, 0.0001d);
+    }
+
+    @Test
+    public void newSigma_testTwo_returnsDouble() throws Exception {
+        double expected = 0.06;
+        double actual = calculator.newSigma(1655.18165406207357949, 2.01476187242, 4952.18257166890839, -5.62682, 0.75, 0.000001);
+
+        //delta = 1655.18165406207357949
+        //v = 4952.18257166890839
+        //phi = 2.01476187242
+        //a = -10.231991619
+        //tau = 0.75
+        //convergence = 0.000001
+
+        assertEquals(expected, actual, 0.0001d);
+    }
+
+    @Test
+    public void newSigma_testThree_returnsDouble() throws Exception {
+        double expected = 0.05999;
+        double actual = calculator.newSigma(-0.4834, 1.1513, 1.7785, -5.62682, 0.5, 0.000001);
+
+        assertEquals(expected, actual, 0.00001d);
+    }
+
+
 }
